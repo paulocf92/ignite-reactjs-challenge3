3973a721c424338ffff0d4f2e6c9e1183446449e
Paulo Filho <pcf.feat@gmail.com>
2021-10-10 20:08:15 -0400

    app: initial scaffold with header and static posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,59 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home | Space Traveling</title>
+      </Head>
+
+      <main className={commonStyles.contentContainer}>
+        <div className={styles.posts}>
+          <section className={styles.post}>
+            <strong>Como utilizar hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <span>
+                <FiCalendar color="var(--info)" size={20} /> 15 Mar 2021
+              </span>
+              <span>
+                <FiUser color="var(--info)" size={20} /> Joseph Oliveira
+              </span>
+            </div>
+          </section>
+          <section className={styles.post}>
+            <strong>Como utilizar hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <span>15 Mar 2021</span>
+              <span>Joseph Oliveira</span>
+            </div>
+          </section>
+          <section className={styles.post}>
+            <strong>Como utilizar hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <span>15 Mar 2021</span>
+              <span>Joseph Oliveira</span>
+            </div>
+          </section>
+          <section className={styles.post}>
+            <strong>Como utilizar hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <span>15 Mar 2021</span>
+              <span>Joseph Oliveira</span>
+            </div>
+          </section>
+        </div>
+        <a href="/" className={styles.loadMore}>
+          Carregar mais posts
+        </a>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
