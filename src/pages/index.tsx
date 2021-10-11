6fb3d2b83d90df9d238267efb1e107d17b4e83f9
Paulo Filho <pcf.feat@gmail.com>
2021-10-11 12:32:50 -0400

    feat: add pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { FiCalendar, FiUser } from 'react-icons/fi';
 
+import { useCallback, useState } from 'react';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -29,8 +30,38 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
+const formatPost = (post: Post): Post => ({
+  uid: post.uid,
+  first_publication_date: format(
+    new Date(post.first_publication_date),
+    'dd MMM Y',
+    {
+      locale: ptBR,
+    }
+  ),
+  data: {
+    title: post.data.title,
+    subtitle: post.data.subtitle,
+    author: post.data.author,
+  },
+});
+
 export default function Home({ postsPagination }: HomeProps): JSX.Element {
-  const { next_page, results } = postsPagination;
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+
+  const loadMorePosts = useCallback(() => {
+    fetch(nextPage)
+      .then(res => res.json())
+      .then(json => {
+        const formattedData: Post[] = json.results.map((post: Post) =>
+          formatPost(post)
+        );
+        console.log(json);
+        setPosts([...posts, ...formattedData]);
+        setNextPage(json.next_page);
+      });
+  }, [nextPage, posts]);
 
   return (
     <>
@@ -40,7 +71,7 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
 
       <main className={commonStyles.contentContainer}>
         <div className={styles.posts}>
-          {results.map(post => (
+          {posts.map(post => (
             <section className={styles.post} key={post.uid}>
               <strong>{post.data.title}</strong>
               <p>{post.data.subtitle}</p>
@@ -56,9 +87,15 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
             </section>
           ))}
         </div>
-        <a href="/" className={styles.loadMore}>
-          Carregar mais posts
-        </a>
+        {nextPage && (
+          <button
+            type="button"
+            className={styles.loadMore}
+            onClick={loadMorePosts}
+          >
+            Carregar mais posts
+          </button>
+        )}
       </main>
     </>
   );
@@ -70,25 +107,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const { next_page, results: res } = postsResponse;
 
-  const results = res.map((post: Post) => {
-    const postData = {
-      uid: post.uid,
-      first_publication_date: format(
-        new Date(post.first_publication_date),
-        'dd MMM Y',
-        {
-          locale: ptBR,
-        }
-      ),
-      data: {
-        title: post.data.title,
-        subtitle: post.data.subtitle,
-        author: post.data.author,
-      },
-    };
-
-    return postData;
-  });
+  const results = res.map((post: Post) => formatPost(post));
 
   return {
     props: {
